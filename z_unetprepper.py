d935f18d4b49f6d8e438cedeb37a5fb17be80f22
jasonccai <61073098+jasonccai@users.noreply.github.com>
2020-02-18 11:42:24 -0600

    Add files via upload

--- z_unetprepper.py
+++ z_unetprepper.py
@@ -35,7 +35,7 @@ def sort(root, images, labels, TVsplit, nb_classes):
     splitposition = int(len(totalset)*TVsplit)
     train = totalset[splitposition:]
     validate = totalset[:splitposition]
-    print("TVsplit:", TVsplit, "Training:", len(train), "Validation:", len(validate))
+    print("Training-Validation Split:", TVsplit, "Training:", len(train), "Validation:", len(validate))
 
     # pickle files to disk
     for each in train:
